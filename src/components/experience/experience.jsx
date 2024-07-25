b27440618c83e46a1102ac3cde48de132743e71b
haard04 <haardshah04@gmail.com>
2024-07-25 13:43:52 +0530

    updated

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -7,8 +7,8 @@ const data=[
     Company: "Marwiz Tech Pvt. Ltd. - Vadodata, GJ, India.",
     Role:"Full Stack Development Intern",
     Duration:"June 2024- July 2024",
-    description1:"➔ Worked on project, where developed an software for licencing and certification renewal and issuing by connecting with agents.",
-    description2:"➔ Tech - Node Js,Express Js, React Js & MongoDb."
+    description1:"➔ Developed software for licensing and certification renewal, connecting companies with agents. Enabled agents to design custom certificates and automate mass generation.",
+    description2:"➔ Technologies used: Node.js, Express.js, React.js, Konva.js, MongoDB."
     
   },
   {
@@ -16,8 +16,8 @@ const data=[
       Company: "Welcome Broker - Ahmedabad, GJ, India.",
       Role:"Software Developing Intern",
       Duration:"May 2022-May 2023",
-      description1:"➔ Using Flutter, Worked on project where implemented features like sauda management which keeps track of the deals of the businesses associated with Welcome Broker and been used by 500+ users worldwide. ",
-      description2:"➔ Added features like summary PDF, Billing,Statistical visualization for better understanding of sales. Developed backend with NodeJs and used MongoDB as database.",
+      description1:"➔ Developed a comprehensive Flutter-based deal management application for Welcome Broker, enabling efficient tracking of business deals and serving over 1,000 users globally. ",
+      description2:"➔ Technologies used: Flutter, Node.js, Express.js, MongoDB, Docker, AWS EC2.",
     },
     
     
--- src/components/header/headerSocials.jsx
+++ src/components/header/headerSocials.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub,BsTwitter} from 'react-icons/bs'
-import {SiCodeforces} from 'react-icons/si'
+import {SiLeetcode} from 'react-icons/si'
 
 const headerSocial = () => {
   return (
@@ -9,7 +9,7 @@ const headerSocial = () => {
         <a href='https://www.linkedin.com/in/haard-/'target="_blank"><BsLinkedin/></a>
         
         <a href='https://github.com/haard04'target='_blank'><BsGithub/></a>
-        <a href='https://codeforces.com/profile/haardshah04'target='_blank'><SiCodeforces/></a>
+        <a href='https://leetcode.com/haardshah04'target='_blank'><SiLeetcode/></a>
         <a href='https://twitter.com/haardshah10'target='_blank'><BsTwitter/></a>
         
     </div>
