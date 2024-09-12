1b81f6e7e74a1dec76f117a86308c143ecf770c1
haard04 <haardshah04@gmail.com>
2024-09-12 12:16:10 +0530

    css updated and content too

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -11,51 +11,71 @@ const data=[
     id:1,
     image:careercatalyst,
     title:<h2>Career Catalyst</h2>,
-    description:<h3>➔ Created a dynamic platform offering resume version control, comprehensive job application tracking, and personalized job recommendations, streamlining the job search process and empowering users with data-driven insights for optimal career decisions.
-   <br/> ➔ Tech: Django, Next Js, AWS RDS (MySQL), NLP</h3>,
+    description:<h3>➔ Engineered the backend using Django and AWS RDS (MySQL) for a dynamic job application tracking platform, designing REST APIs and optimizing database queries, which reduced data retrieval time by 60%.
+   <br/>➔ Created a responsive platform for comprehensive job application tracking and personalized job recommendations,
+   enhancing the job search process and offering users data-driven insights for better career decisions.
+   <br/>➔ Prepared the frontend with React.js and integrated NLP, improving job match relevance by 57%.
+   </h3>,
     github:"https://github.com/haard04/careercatalyst",
     demo:"https://github.com/haard04/careercatalyst"
-  },
-  {
-    id:2,
-    image:expensesmeter,
-    title:<h2>Expenses Meter</h2>,
-    description:<h3>➔ Designed, built, and deployed a MERN stack based web application which gives user a platform where he can manage expenses, Incomes, Balances with Chart Visulization.<br/>➔ Set up a database with MongoDB Atlas to store , view, Edit and delete functionality.<br/>➔ Tech: MongoDB, React Js, Express Js, Node Js</h3>,
-    github:"https://github.com/haard04/Expensometer",
-    demo:"https://expensesmeter.netlify.app/"
   },
   {
     id:3,
     image:algo,
     title:<h2>OS Algorithm simulator</h2> ,
-    description:<h3>➔ Using Flutter created application as a group project in which you can simulate operating system’s Algorithms like CPU scheduling, Disk scheduling, Page replacement, Deadlock condition etc. <br/>➔ Used Technologies: Flutter,Dart, C++,OpenAI API</h3>,
+    description:<h3>
+      ➔ Engineered a user-friendly Flutter app with MongoDB to simplify concepts of OS algorithms for students and
+      professionals, focusing on detailed simulations of CPU scheduling, disk scheduling, page replacement, and deadlock.
+      <br/>
+      ➔ Created a comprehensive platform to visualize OS algorithms, enhancing understanding through detailed representations
+of key concepts.
+<br/>
+➔ Integrated OpenAI API for a chatbot feature, enhancing learning and increasing user engagement by 35%.
+      </h3>,
     github:"https://github.com/haard04/CPU_schedule",
     demo:"https://github.com/haard04/CPU_schedule"
   },
   {
+    id:2,
+    image:null,
+    title:<h2>RideSecure</h2>,
+    description:<h3>
+      ➔ Implemented RideSecure, a computer vision project using YOLOv8 to detect triple riders, helmet compliance, and
+      mobile device usage on motorcycles with 95% accuracy.
+      <br/>
+      <br/>
+      ➔ Achieved high-performance detection with a custom dataset of over 6,000 images, enhancing road safety through
+real-time monitoring.
+    </h3>,
+    github:"https://github.com/haard04/RideSecure",
+    demo:"https://github.com/haard04/cliniflow-app"
+  },
+  
+  {
     id:4,
     image:musiIMG,
     title:<h2>MusiPedia </h2>,
-    description:<h3>➔ Designed, built, and deployed a MERN stack-based web application which gives user a platform where he can share story or details about a song .<br/> ➔ Set up a database with MongoDB Atlas to store , view, Edit and delete functionality.<br/> ➔ Tech: MongoDB, ReactJs, ExpressJs, NodeJs.</h3>,
+    description:<h3>➔ Designed, built, and deployed a MERN stack-based web application which gives user a platform where he can share story or details about a song .<br/> <br/>➔ Set up a database with MongoDB Atlas to store , view, Edit and delete functionality.<br/><br/> ➔ Tech: MongoDB, ReactJs, ExpressJs, NodeJs.</h3>,
     github:"https://github.com/haard04/musipedia",
     demo:"https://musipediaaa.netlify.app/"
   },
   {
     id:5,
+    image:expensesmeter,
+    title:<h2>Expenses Meter</h2>,
+    description:<h3>➔ Designed, built, and deployed a MERN stack based web application which gives user a platform where he can manage expenses, Incomes, Balances with Chart Visulization.<br/><br/>➔ Set up a database with MongoDB Atlas to store , view, Edit and delete functionality.<br/><br/>➔ Tech: MongoDB, React Js, Express Js, Node Js</h3>,
+    github:"https://github.com/haard04/Expensometer",
+    demo:"https://expensesmeter.netlify.app/"
+  },
+  {
+    id:6,
     image:HMS,
     title:<h2>Hospital Management System</h2>,
-    description:<h3>➔ Worked on Hospital management system as Database Management System project.<br/>➔ Tech: MySQL </h3>,
+    description:<h3>➔ Worked on Hospital management system as Database Management System project.<br/><br/>➔ Tech: MySQL </h3>,
     github:"https://github.com/haard04/Hospital-Management-System---DBMS-project",
     demo:"https://github.com/haard04/Hospital-Management-System---DBMS-project"
   },
-  // {
-  //   id:7,
-  //   image:null,
-  //   title:"Cliniflow App",
-  //   description:"Created Rescue management System as Part of Hackout-2023 at DAIICT,Gandhinagar, Here Doctor do not need to write all the prescription writing manually, instead doctor just have to speak and it will generate PDF of prescription, It will make ease the process.Tech stack: Flutter,Node Js,Express Js, MongoDb",
-  //   github:"hhttps://github.com/haard04/cliniflow-app",
-  //   demo:"https://github.com/haard04/cliniflow-app"
-  // },
+  
 ]
 
 const Project = () => {
@@ -68,14 +88,14 @@ const Project = () => {
           data.map(({id,image,title,description,github,demo})=>{
             return (
               <article key={id} className='project__item'>
-        <div className='project__item-image'>
+        {/* <div className='project__item-image'>
         <img src={image} alt={title}></img>
-        </div>
-        <h3>{title}</h3>
+        </div> */}
+        <center className='project__title'>{title}</center>
         <h5 className='project_desc'>{description}</h5>
         <div className='project__item-cta'>
-        <a href={github} className='btn' target={'_blank'}>Github</a>
-        <a href={demo} className='btn btn-primary' target={'_blank'}>Live Demo</a>
+        <a href={github} className='project__button' target={'_blank'}>Github</a>
+        {/* <a href={demo} className='btn btn-primary' target={'_blank'}>Live Demo</a> */}
         </div>
         
         </article>
--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -4,20 +4,22 @@ import './experience.css';
 const data=[
   {
     id:1,
-    Company: "Marwiz Tech Pvt. Ltd. - Vadodata, GJ, India.",
-    Role:"Full Stack Development Intern",
-    Duration:"June 2024- July 2024",
-    description1:"➔ Developed software for licensing and certification renewal, connecting companies with agents. Enabled agents to design custom certificates and automate mass generation.",
-    description2:"➔ Technologies used: Node.js, Express.js, React.js, Konva.js, MongoDB."
+    Company: "Marwiz Tech Pvt. Ltd. - Vadodara, Gujarat, India",
+    Role:"Full Stack Web and App Development Intern",
+    Duration:"June 2024 - July 2024",
+    description1:"➔ Developed the backend for the ”EHS Mind Manager”, a licensing and certification renewal platform, using Node.js and Express.js, reducing the time to issue new certificates by 78%.",
+    description2:"➔ Built the frontend with React.js for the web and Flutter for the app, enabling customizable design in the canvas and improving design efficiency by 54%.",
+    description3:"➔ Utilized MongoDB aggregations for efficient data management in the platform, increasing exploration efficiency by 97% and speeding up expiration date searches."
     
   },
   {
       id:2,
-      Company: "Welcome Broker - Ahmedabad, GJ, India.",
-      Role:"Software Developing Intern",
-      Duration:"May 2022-May 2023",
-      description1:"➔ Developed a comprehensive Flutter-based deal management application for Welcome Broker, enabling efficient tracking of business deals and serving over 1,000 users globally. ",
-      description2:"➔ Technologies used: Flutter, Node.js, Express.js, MongoDB, Docker, AWS EC2.",
+      Company: "Welcome Broker - Ahmedabad, Gujarat, India.",
+      Role:"Software Development Intern",
+      Duration:"May 2022 - May 2023",
+      description1:"➔ Engineered a business management application using Flutter for both Android and iOS, featuring personalized sales analysis, order management, and tracking systems. It increased order accuracy from 73.42% to 99.94%.",
+      description2:"➔ Built the backend using NodeJs and MongoDB, optimizing the invoicing service with Redis-based caching, which improved response times by 90%. Deployed the application using Docker on AWS.",
+      description3:"➔ Programmed a notification system for timely user updates and real-time alerts on financial transactions and delivery status, increasing daily active users (DAU) by 43% over 3 months."
     },
     
     
@@ -31,7 +33,7 @@ const Experience = () => {
 
         <div className='Experience'>
         {
-          data.map(({id,Company,Role,Duration,description1,description2})=>{
+          data.map(({id,Company,Role,Duration,description1,description2,description3})=>{
             return (
                 <article key={id} className='exp_item'>
                   <div className='company-duration'>
@@ -41,6 +43,7 @@ const Experience = () => {
                   <h3 className='role'>{Company}</h3>
                   <h5 className='exp_desc'>{description1}</h5>
                   <h5 className='exp_desc'>{description2}</h5>
+                  <h5 className='exp_desc'>{description3}</h5>
                 </article>
               )
               
